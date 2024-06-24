01fce2494d4aaf09ec1890100e784af56a991055
Simon Scoltock <sscoltock@simplemachines.com.au>
2024-06-24 20:16:25 +1200

    api echos values

---
+++ ingress-api/app/main.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/event/{sensor_id}")
+def receive_event(sensor_id: str, temp: float = None, moisture: float = None, humidity: float = None):
+    return {"sensor_id": sensor_id, "temperature":temp, "moist":moisture, "humidity":humidity}
